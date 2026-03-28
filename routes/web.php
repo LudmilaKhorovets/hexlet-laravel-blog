7414b612a120daa04dd9e821cc15bba8300e98a1
Ludmila <ludakhorovets@tut.by>
2026-03-28 15:13:12 +0300

    new controller

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return view('about');
-})->name('about');
+Route::get('about', [PageController::class, 'about'])->name('about');
